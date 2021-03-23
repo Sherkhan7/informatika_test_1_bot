eef4e3d04c2491532fce84de59db348fa6be0ec1
sherkhan7 <9669470@gmail.com>
2021-03-23 16:30:41 +0500

    upgrade PTB to 13.4.1v and Pillow to 8.1.2

--- main_iformatika_test_1.py
+++ main_iformatika_test_1.py
@@ -18,8 +18,7 @@ def main():
     # updater.start_polling()
     # updater.idle()
 
-    updater.start_webhook(listen='127.0.0.1', port=5008, url_path=TOKEN)
-    updater.bot.set_webhook(url='https://cardel.ml/' + TOKEN)
+    updater.start_webhook(listen='127.0.0.1', port=5008, url_path=TOKEN, webhook_url='https://cardel.ml/' + TOKEN)
     updater.idle()
 
 
